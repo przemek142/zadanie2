6b744c03b07d033ae5a23038b2566acae6bc543c
Przemysław Sitek <przemo2@gmail.com>
2018-09-13 16:40:49 +0200

    Poprawione: - klasy z Main(): Cars, Zad2, - text: zadanie-java-32

--- src/Cars.java
+++ src/Cars.java
@@ -1,16 +1,22 @@
 class Cars {
     public static void main(String[] args) {
+
+
+
+
         Car car1 = new Car("BMW", "M5", 2007, 2.6); //marka, model, rocznik, pojemność silnika
         Car car2 = new Car("Opel", "Vectra", 2004, 1.8); //marka, model, rocznik, pojemność silnika
 
-//        System.out.println("Samochód 1:");
-//        System.out.println("Marka: " + car1.brand + ", model: " + car1.model +
-//                ", rocznik: " + car1.year + ", pojemność: " + car1.capacity);
-//        System.out.println("Samochód 2:");
-//        System.out.println("Marka: " + car2.brand + ", model: " + car2.model +
-//                ", rocznik: " + car2.year + ", pojemność: " + car2.capacity);
+        System.out.println("Samochód 1:");
+        System.out.println("Marka: " + car1.brand + ", model: " + car1.model +
+                ", rocznik: " + car1.year + ", pojemność: " + car1.capacity);
+        System.out.println("Samochód 2:");
+        System.out.println("Marka: " + car2.brand + ", model: " + car2.model +
+                ", rocznik: " + car2.year + ", pojemność: " + car2.capacity);
 
-        System.out.printf("Samochód 1:\nMarka: %s, model: %s, rocznik: %d, pojemność: %.1f\n",car1.brand, car1.model, car1.year, car1.capacity );
+        // Wersja printf
+        System.out.println("\n\nwersja printf:");
+        System.out.printf("Samochód 1:\nMarka: %s, model: %s, rocznik: %d, pojemność: %.1f\n",car1.brand, car1.model, car1.year, car1.capacity);
         System.out.printf("Samochód 1:\nMarka: %s, model: %s, rocznik: %d, pojemność: %.1f\n",car2.brand, car2.model, car2.year, car2.capacity );
     }
 }
--- src/Zad2.java
+++ src/Zad2.java
@@ -1,32 +1,32 @@
-//public class Zad2 {
-//    public static void main(String[] args) {
-//
-//        Chair armChair = new Chair();
-//        Chair stool = new Chair();
-//
-//        armChair.colorOfChair="white";
-//        armChair.hasBackSupport=true;
-//        armChair.numerOfLegs=4;
-//        stool.colorOfChair="black";
-//        stool.hasBackSupport=false;
-//        stool.numerOfLegs=3;
-//
-//        String text1, text2;
-//
-//        if (armChair.hasBackSupport){
-//            text1 = "has back support";
-//        } else {
-//            text1 = "doesn't have back support";
-//        }
-//
-//        if (stool.hasBackSupport){
-//            text2 = "has back support";
-//        } else {
-//            text2 = "doesn't have back support";
-//        }
-//
-//
-//        System.out.printf("My armchair has %d legs, is %s and %s.\n", armChair.numerOfLegs, armChair.colorOfChair, text1 );
-//        System.out.printf("My stool has %d legs, is %s and %s.\n", stool.numerOfLegs, stool.colorOfChair, text2 );
-//    }
-//}
+public class Zad2 {
+    public static void main(String[] args) {
+
+        Chair armChair = new Chair();
+        Chair stool = new Chair();
+
+        armChair.colorOfChair="white";
+        armChair.hasBackSupport=true;
+        armChair.numerOfLegs=4;
+        stool.colorOfChair="black";
+        stool.hasBackSupport=false;
+        stool.numerOfLegs=3;
+
+        String text1, text2;
+
+        if (armChair.hasBackSupport){
+            text1 = "has back support";
+        } else {
+            text1 = "doesn't have back support";
+        }
+
+        if (stool.hasBackSupport){
+            text2 = "has back support";
+        } else {
+            text2 = "doesn't have back support";
+        }
+
+
+        System.out.printf("My armchair has %d legs, is %s and %s.\n", armChair.numerOfLegs, armChair.colorOfChair, text1 );
+        System.out.printf("My stool has %d legs, is %s and %s.\n", stool.numerOfLegs, stool.colorOfChair, text2 );
+    }
+}
